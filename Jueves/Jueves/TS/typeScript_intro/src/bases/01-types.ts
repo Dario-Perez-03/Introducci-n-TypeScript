efdfb97659b6690908e9779100e464c7295e4d6a
Dario-Perez-03 <juandariopt@gmail.com>
2025-06-12 14:06:31 -0500

    Delete Jueves/Jueves directory

--- Jueves/Jueves/TS/typeScript_intro/src/bases/01-types.ts
+++
@@ -1,3 +0,0 @@
-
-export const name: string = "dp";
-console.log(name);
--- Jueves/Jueves/proyecto/ApiViernes/src/routes/route.excel.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { generarExcel } from '../libs/excel.js';
-
-const router = Router();
-
-router.get("/generar-excel", generarExcel);
-
-export default router;
